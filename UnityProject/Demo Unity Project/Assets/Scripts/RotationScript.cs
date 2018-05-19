b52258375a56500a6893783bfa4136771cf0a68c
Dakota Herold <dakota.herold@yahoo.com>
2018-05-18 20:20:24 -0400

    Added rotation script and presentation scene.

---
+++ UnityProject/Demo Unity Project/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    public float rotationAngle = 20f; 
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, rotationAngle * Time.deltaTime, 0)); 
+	}
+}
