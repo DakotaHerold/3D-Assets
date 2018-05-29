4fc71dd8d2e1805a8b4bd8ccab9b82697cf0de88
Dakota Herold <dakota.herold@yahoo.com>
2018-05-28 22:58:25 -0400

    Chair Model. Updated rotation script. Added chair scene.

--- UnityProject/Demo Unity Project/Assets/Scripts/RotationScript.cs
+++ UnityProject/Demo Unity Project/Assets/Scripts/RotationScript.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour {
 
+    public enum Axis
+    {
+        X, 
+        Y,
+        Z
+    }
+
+    public Axis rotationAxis = Axis.Y; 
     public float rotationAngle = 20f; 
 
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(0, rotationAngle * Time.deltaTime, 0)); 
+
+        switch(rotationAxis)
+        {
+            case Axis.X:
+                transform.Rotate(new Vector3(rotationAngle * Time.deltaTime, 0, 0));
+                break;
+            case Axis.Y:
+                transform.Rotate(new Vector3(0, rotationAngle * Time.deltaTime, 0));
+                break;
+            case Axis.Z:
+                transform.Rotate(new Vector3(0, 0, rotationAngle * Time.deltaTime));
+                break; 
+        }
+
+        
 	}
 }
